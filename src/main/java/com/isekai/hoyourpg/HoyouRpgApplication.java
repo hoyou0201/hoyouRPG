0e66cf98ee22cca1e05fd9b3ca37b7f88c2552b6
ChoiHoSeong <chs3112a@gmail.com>
2025-12-25 09:50:46 +0000

    chore: init spring boot project

---
+++ src/main/java/com/isekai/hoyourpg/HoyouRpgApplication.java
@@ -0,0 +1,13 @@
+package com.isekai.hoyourpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoyouRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoyouRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isekai/hoyourpg/HoyouRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isekai.hoyourpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoyouRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
