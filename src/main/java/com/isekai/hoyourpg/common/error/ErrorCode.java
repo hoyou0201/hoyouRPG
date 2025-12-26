6c959aee9e5890137ddec93514e23a3e33d2fc00
ChoiHoSeong <chs3112a@gmail.com>
2025-12-26 05:44:07 +0000

    feat: add Exp

--- src/main/java/com/isekai/hoyourpg/common/error/ErrorCode.java
+++ src/main/java/com/isekai/hoyourpg/common/error/ErrorCode.java
@@ -2,4 +2,5 @@ package com.isekai.hoyourpg.common.domain.error;
 
 public enum ErrorCode {
     LEVEL_OUT_OF_RANGE,
+    EXP_NEGATIVE,
 }
---
+++ src/main/java/com/isekai/hoyourpg/common/vo/Exp.java
@@ -0,0 +1,28 @@
+package com.isekai.hoyourpg.common.domain.vo;
+
+import com.isekai.hoyourpg.common.domain.error.DomainException;
+import com.isekai.hoyourpg.common.domain.error.ErrorCode;
+
+public final class Exp {
+    private final long value;
+
+    public Exp(long value) {
+        if (value < 0) {
+            throw new DomainException(ErrorCode.EXP_NEGATIVE, "경험치는 음수일 수 없습니다.");
+        }
+        this.value = value;
+    }
+
+    public long value() {
+        return value;
+    }
+
+    public Exp add(Exp other) {
+        return new Exp(this.value + other.value);
+    }
+
+    public Exp subtract(long amount) {
+        long next = this.value - amount;
+        return new Exp(Math.max(next, 0)); //exp 최소값 0
+    }
+}
