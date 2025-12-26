dec5020c6be34dc4a0d320e7f84d15bb07b89c3d
ChoiHoSeong <chs3112a@gmail.com>
2025-12-26 05:26:21 +0000

    feat: add Level value object

--- src/main/java/com/isekai/hoyourpg/common/error/ErrorCode.java
+++ src/main/java/com/isekai/hoyourpg/common/error/ErrorCode.java
@@ -1,5 +1,5 @@
 package com.isekai.hoyourpg.common.domain.error;
 
 public enum ErrorCode {
-    
+    LEVEL_OUT_OF_RANGE,
 }
---
+++ src/main/java/com/isekai/hoyourpg/common/vo/Level.java
@@ -0,0 +1,23 @@
+package com.isekai.hoyourpg.common.domain.vo;
+
+import com.isekai.hoyourpg.common.domain.error.DomainException;
+import com.isekai.hoyourpg.common.domain.error.ErrorCode;
+
+public class Level {
+    private final int value;
+
+    public Level(int value){
+        if (value < 1) {
+            throw new DomainException(ErrorCode., "레벨은 1 이상이어야 합니다.");
+        }
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
+
+    public Level next() {
+        return new Level(value + 1);
+    }
+}
