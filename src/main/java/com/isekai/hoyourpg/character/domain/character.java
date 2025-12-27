a330d1efb1603c6c09c5e3a02b99c3c3073203c7
ChoiHoSeong <chs3112a@gmail.com>
2025-12-27 06:59:54 +0000

    feat: add character's stats

--- src/main/java/com/isekai/hoyourpg/character/domain/character.java
+++ src/main/java/com/isekai/hoyourpg/character/domain/character.java
@@ -1,1 +1,27 @@
-package com.isekai.hoyourpg;
+// character/domain/Character.java
+package com.isekai.hoyourpg.character.domain;
+
+import com.isekai.hoyourpg.common.domain.vo.Exp;
+import com.isekai.hoyourpg.common.domain.vo.Level;
+import com.isekai.hoyourpg.common.domain.vo.Name;
+
+import java.util.Objects;
+
+public class Character {
+    private Name name;
+    private Level level;
+    private Exp exp;
+
+    public Character(CharacterId id, Name name) {
+        this.id = Objects.requireNonNull(id);
+        this.name = Objects.requireNonNull(name);
+        this.level = new Level(1);
+        this.exp = new Exp(0);
+    }
+
+    
+    public Name name() { return name; }
+    public Level level() { return level; }
+    public Exp exp() { return exp; }
+    
+}
