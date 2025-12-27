05acb6a679fe5e94047b4a29fe5a2e371ba509ac
ChoiHoSeong <chs3112a@gmail.com>
2025-12-27 06:40:22 +0000

    feat: add Hp vo

--- src/main/java/com/isekai/hoyourpg/common/error/ErrorCode.java
+++ src/main/java/com/isekai/hoyourpg/common/error/ErrorCode.java
@@ -3,5 +3,6 @@ package com.isekai.hoyourpg.common.domain.error;
 public enum ErrorCode {
     LEVEL_OUT_OF_RANGE,
     EXP_NEGATIVE,
+    HP_NEGATIVE,
     INVALID_NAME,
 }
---
+++ src/main/java/com/isekai/hoyourpg/common/vo/Hp.java
@@ -0,0 +1,28 @@
+package com.isekai.hoyourpg.common.domain.vo;
+
+import com.isekai.hoyourpg.common.domain.error.DomainException;
+import com.isekai.hoyourpg.common.domain.error.ErrorCode;
+
+public final class Hp {
+    private final int value;
+
+    public Hp(int value) {
+        if (value < 0) {
+            throw new DomainException(ErrorCode.Hp_NEGATIVE, "체력는 음수일 수 없습니다.");
+        }
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
+
+    public Hp add(Hp other) {
+        return new Hp(this.value + other.value);
+    }
+
+    public Hp subtract(int amount) {
+        int next = this.value - amount;
+        return new Hp(Math.max(next, 0)); //int 최소값 0
+    }
+}
