6343e96e431407544679e75580952d1ab3be3a84
ChoiHoSeong <chs3112a@gmail.com>
2025-12-28 06:26:49 +0000

    refacter: change jobcode field string to JobCode vo

--- src/main/java/com/isekai/hoyourpg/character/domain/Job/Job.java
+++ src/main/java/com/isekai/hoyourpg/character/domain/Job/Job.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public record Job(
     JobId id,
     String name,
-    String code,
+    JobCode code,
     JobTier tier,
     Set<JobId> nextJobs,
     Set<SkillId> learnablePassives,
