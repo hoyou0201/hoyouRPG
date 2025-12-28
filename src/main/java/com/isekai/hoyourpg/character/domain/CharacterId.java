82b20c023a80409b440a363eab3d8c2c713bed61
ChoiHoSeong <chs3112a@gmail.com>
2025-12-28 07:08:30 +0000

    feat: add characterid

---
+++ src/main/java/com/isekai/hoyourpg/character/domain/CharacterId.java
@@ -0,0 +1,16 @@
+package com.isekai.hoyourpg.character.domain;
+
+import java.util.Objects;
+import java.util.UUID;
+
+
+public record CharacterId(UUID id) {
+    public CharacterId {
+        Object.requireNonNull(id);
+    }
+
+    public static CharacterId of(UUID id) {
+        return new CharacterId(id);
+    }
+
+}
