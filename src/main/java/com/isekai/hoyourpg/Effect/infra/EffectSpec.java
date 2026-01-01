8091cf920cf667d40a762844b2f95aa9d7459c0e
ChoiHoSeong <chs3112a@gmail.com>
2026-01-01 10:29:50 +0000

    chore: add effectspec

---
+++ src/main/java/com/isekai/hoyourpg/Effect/infra/EffectSpec.java
@@ -0,0 +1,16 @@
+package com.isekai.hoyourpg.Effect.infra;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+
+
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type"
+)
+@JsonSubTypes({
+    @Type(value = DealDamegeSpec.class, name = "dealDamage")
+})
+public interface EffectSpec {}
