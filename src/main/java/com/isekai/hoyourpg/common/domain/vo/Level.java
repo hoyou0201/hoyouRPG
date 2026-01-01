82dfce391d317eda6c15644abe7c8f60d32feb49
ChoiHoSeong <chs3112a@gmail.com>
2026-01-01 08:22:50 +0000

    refacter: change level record

--- src/main/java/com/isekai/hoyourpg/common/domain/vo/Level.java
+++ src/main/java/com/isekai/hoyourpg/common/domain/vo/Level.java
@@ -3,18 +3,12 @@ package com.isekai.hoyourpg.common.domain.vo;
 import com.isekai.hoyourpg.common.domain.error.DomainException;
 import com.isekai.hoyourpg.common.domain.error.ErrorCode;
 
-public class Level {
-    private final int value;
+public record Level(int value) {
 
     public Level(int value){
         if (value < 1) {
             throw new DomainException(ErrorCode.LEVEL_OUT_OF_RANGE, "레벨은 1 이상이어야 합니다.");
         }
-        this.value = value;
-    }
-
-    public int value() {
-        return value;
     }
 
     public Level next() {
