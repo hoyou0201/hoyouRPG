69c48cb3fd1e8111e404df571253f65740c1992d
ChoiHoSeong <chs3112a@gmail.com>
2026-01-01 09:04:31 +0000

    feat: add skill

--- src/main/java/com/isekai/hoyourpg/character/domain/Skill/Skill.java
+++ src/main/java/com/isekai/hoyourpg/character/domain/Skill/Skill.java
@@ -1,7 +1,12 @@
 package com.isekai.hoyourpg.character.domain.Skill;
 
+import java.util.Set;
+
+import com.isekai.hoyourpg.character.domain.Effect.Effect;
+
 public record Skill(
     SkillId id,
     String name,
-    SkillCode code) {
+    SkillCode code,
+    Set<Effect> effects) {
 }
