3085dfd412b49f92c31c5a91d11219756fd1ae59
ChoiHoSeong <chs3112a@gmail.com>
2026-01-11 14:19:13 +0000

    feat: add null exception

--- src/main/java/com/isekai/hoyourpg/Effect/infra/EffectFactory.java
+++ src/main/java/com/isekai/hoyourpg/Effect/infra/EffectFactory.java
@@ -5,6 +5,7 @@ import com.isekai.hoyourpg.Effect.domain.*;
 public class EffectFactory{
     
     public Effect toEffect(EffectSpec spec){
+        Objects.requireNonNull(spec, "spec");
         return switch(spec){
             case DealDamageSpec s -> 
                 new DealDamage(s.amount());
